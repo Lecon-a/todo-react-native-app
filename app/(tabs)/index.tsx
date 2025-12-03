2c2d1d19469b4ae9315a2a70fbf09c0cf1c98bd9
Sunday P. Afolabi <spog4true@gmail.com>
2025-12-03 01:44:32 -0800

    implement header progress bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import useTheme, { ColorScheme } from '@/hooks/useTheme'
+import { Text, TouchableOpacity, StatusBar } from 'react-native'
+import useTheme from '@/hooks/useTheme'
 import { api } from "@/convex/_generated/api";
 import { useMutation, useQuery } from "convex/react";
 import { createHomeStyles } from '@/assets/styles/homo.styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
+import Header from '@/components/Header';
+
 
 const Home = () => {
 
@@ -11,12 +14,18 @@ const Home = () => {
   const homeStyles = createHomeStyles(colors)
 
   return (
-    <SafeAreaView style={homeStyles.container}>
-      <TouchableOpacity style={''} onPress={toggleDarkMode}>
-        <Text>Toggle</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  )
-}
+    <LinearGradient
+      colors={colors.gradient.background}
+      style={homeStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.container}>
+        <Header />
+        <TouchableOpacity style={''} onPress={toggleDarkMode}>
+          <Text>Toggle</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
+)}
 
 export default Home
